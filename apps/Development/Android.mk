e696b9f709f0041706a93122dfd494bedcba4b43
Paul Beeler <sparksco@gmail.com>
2013-04-09 13:13:55 -0600

    remove eng tag Signed-off-by: Paul Beeler <sparksco@gmail.com>

--- apps/Development/Android.mk
+++ apps/Development/Android.mk
@@ -1,8 +1,6 @@
 LOCAL_PATH:= $(call my-dir)
 include $(CLEAR_VARS)
 
-LOCAL_MODULE_TAGS := eng
-
 LOCAL_JAVA_LIBRARIES := android.test.runner telephony-common
 
 LOCAL_SRC_FILES := $(call all-subdir-java-files) \
